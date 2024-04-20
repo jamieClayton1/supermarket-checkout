be716ad44f62f67b8531e87a418d503a8225c10d
jclaytonawin <107044720+jclaytonawin@users.noreply.github.com>
2024-04-20 12:05:11 +0100

    feat(main): basic log formatting

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -3,9 +3,17 @@ package main
 import (
 	"supermarket-checkout/internal/api"
 	"supermarket-checkout/internal/provider"
+
+	log "github.com/sirupsen/logrus"
 )
 
+func init() {
+	log.SetFormatter(&log.JSONFormatter{})
+}
+
 func main() {
+	log.Info("Generating service provider.")
 	services := provider.NewApiServiceProvider()
+	log.Info("Serving api.")
 	api.NewAPI(&services).Serve()
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,11 @@ go 1.21.3
 require gotest.tools v2.2.0+incompatible
 
 require (
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+)
+
+require (
 	github.com/google/go-cmp v0.6.0 // indirect
 	github.com/gorilla/mux v1.8.1
 	github.com/pkg/errors v0.9.1 // indirect
